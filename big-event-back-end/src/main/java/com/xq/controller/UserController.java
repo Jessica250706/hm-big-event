2266f28ef7400700ead41096d3e0b5b1fb1b4fdb
Jessica Wang <1271736670@qq.com>
2026-08-31 17:54:28 +0800

    feat: 15.新增文章分类

--- big-event-back-end/src/main/java/com/xq/controller/UserController.java
+++ big-event-back-end/src/main/java/com/xq/controller/UserController.java
@@ -9,6 +9,7 @@ import com.xq.utils.ThreadLocalUtil;
 import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -73,10 +74,41 @@ public class UserController {
         return Result.success();
     }
 
-    @PatchMapping("updateAvatar")
+    @PatchMapping("/updateAvatar")
     public Result updateAvatar(@RequestParam @URL String avatarUrl) {
         userService.updateAvatar(avatarUrl);
         return Result.success();
     }
 
+    @PatchMapping("/updatePwd")
+    public Result updatePwd(@RequestBody Map<String, String> params) {
+        // 1.校验参数
+        String oldPwd = params.get("old_pwd");
+        String newPwd = params.get("new_pwd");
+        String rePwd = params.get("re_pwd");
+
+        // 1.1 检验是否为空
+        if (!StringUtils.hasLength(oldPwd) || !StringUtils.hasLength(newPwd) || !StringUtils.hasLength(rePwd)) {
+            return Result.error("缺少必要的参数！");
+        }
+
+        // 1.2 原密码是否正确
+        // 调用 userService 根据用户名拿到原密码，再和 oldPwd 比对
+        Map<String, Object> map = ThreadLocalUtil.get();
+        String username = (String) map.get("username");
+        User loginUser = userService.findByUserName(username);
+        if (!loginUser.getPassword().equals(Md5Util.getMD5String(oldPwd))) {
+            return Result.error("原密码填写不正确！");
+        }
+
+        // 1.3 newPwd 与 rePwd 是否一致
+        if (!rePwd.equals(newPwd)) {
+            return Result.error("两次填写的新密码不一样！");
+        }
+
+        // 2.调用 service 完成密码更新
+        userService.updatePwd(newPwd);
+
+        return Result.success();
+    }
 }
