a5c5e33b75b36dc6a5c360b541ec00daffac71f9
Jessica Wang <1271736670@qq.com>
2026-08-31 17:31:35 +0800

    feat: 13.更新用户头像

--- big-event-back-end/src/main/java/com/xq/controller/UserController.java
+++ big-event-back-end/src/main/java/com/xq/controller/UserController.java
@@ -7,6 +7,7 @@ import com.xq.utils.JwtUtil;
 import com.xq.utils.Md5Util;
 import com.xq.utils.ThreadLocalUtil;
 import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.URL;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -69,6 +70,12 @@ public class UserController {
     @PutMapping("/update")
     public Result update(@RequestBody @Validated User user) {
         userService.update(user);
+        return Result.success();
+    }
+
+    @PatchMapping("updateAvatar")
+    public Result updateAvatar(@RequestParam @URL String avatarUrl) {
+        userService.updateAvatar(avatarUrl);
         return Result.success();
     }
 
--- big-event-back-end/src/main/java/com/xq/service/impl/UserServiceImpl.java
+++ big-event-back-end/src/main/java/com/xq/service/impl/UserServiceImpl.java
@@ -4,10 +4,12 @@ import com.xq.mapper.UserMapper;
 import com.xq.pojo.User;
 import com.xq.service.UserService;
 import com.xq.utils.Md5Util;
+import com.xq.utils.ThreadLocalUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Map;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -33,5 +35,12 @@ public class UserServiceImpl implements UserService {
     public void update(User user) {
         user.setUpdateTime(LocalDateTime.now());
         userMapper.update(user);
+    }
+
+    @Override
+    public void updateAvatar(String avatarUrl) {
+        Map<String, Object> map = ThreadLocalUtil.get();
+        Integer id = (Integer) map.get("id");
+        userMapper.updateAvatar(avatarUrl, id);
     }
 }
