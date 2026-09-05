da4668f9a98d62d5b8a50d66ee856f345aedbe9b
Jessica Wang <1271736670@qq.com>
2026-09-05 21:11:16 +0800

    feat: 70.pinia持久化插件-persist

--- big-event-front-end/src/main.ts
+++ big-event-front-end/src/main.ts
@@ -6,9 +6,12 @@ import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import router from '@/router'
 import { createPinia } from 'pinia'
+import { createPersistedState } from 'pinia-plugin-persistedstate'
 
 const app = createApp(App)
 const pinia = createPinia()
+const persist = createPersistedState()
+pinia.use(persist)
 app.use(pinia)
 app.use(router)
 app.use(ElementPlus)
--- big-event-front-end/src/stores/token.ts
+++ big-event-front-end/src/stores/token.ts
@@ -1,24 +1,29 @@
-// 定义 store
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useTokenStore = defineStore('token', () => {
-  // 1.响应式变量
-  const token = ref<string>('')
+export const useTokenStore = defineStore(
+  'token',
+  () => {
+    // 1.响应式变量
+    const token = ref<string>('')
 
-  // 2.定义一个函数，修改 token 的值
-  const setToken = (newToken: string) => {
-    token.value = newToken
-  }
+    // 2.定义一个函数，修改 token 的值
+    const setToken = (newToken: string) => {
+      token.value = newToken
+    }
 
-  // 3.定义一个函数，移除 token 的值
-  const removeToken = () => {
-    token.value = ''
-  }
+    // 3.定义一个函数，移除 token 的值
+    const removeToken = () => {
+      token.value = ''
+    }
 
-  return {
-    token,
-    setToken,
-    removeToken,
-  }
-})
+    return {
+      token,
+      setToken,
+      removeToken,
+    }
+  },
+  {
+    persist: true, // 持久化存储
+  },
+)
